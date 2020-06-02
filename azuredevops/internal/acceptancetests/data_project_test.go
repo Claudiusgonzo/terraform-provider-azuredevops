4ab523ed4b9c2ce8c4448e3d76ef0353450db1ac
Nick Iodice <niiodice@microsoft.com>
2020-06-02 15:18:22 -0500

    Undo unintended change in acceptance test

--- azuredevops/internal/acceptancetests/data_project_test.go
+++ azuredevops/internal/acceptancetests/data_project_test.go
@@ -44,7 +44,7 @@ func TestAccProject_DataSource(t *testing.T) {
 
 func TestAccProject_DataSource_IncorrectParameters(t *testing.T) {
 	projectName := testutils.GenerateResourceName()
-	DataProject := fmt.Sprintf(`
+	dataProject := fmt.Sprintf(`
 	data "azuredevops_project" "project" {
 		project_name = "%s"
 		description = "A project description"
@@ -57,7 +57,7 @@ func TestAccProject_DataSource_IncorrectParameters(t *testing.T) {
 		Providers: testutils.GetProviders(),
 		Steps: []resource.TestStep{
 			{
-				Config:      DataProject,
+				Config:      dataProject,
 				ExpectError: errorRegex,
 			},
 		},
